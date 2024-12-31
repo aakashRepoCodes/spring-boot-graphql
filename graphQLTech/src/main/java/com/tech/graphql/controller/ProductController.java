9c43cb8f04931c26eb46b0e32ec820f82a6945e7
Aakash Chaturvedi <aakashch@mail.uni-paderborn.de>
2024-12-31 15:29:45 +0100

    exposed entity schema with graphQL

--- graphQLTech/src/main/java/com/tech/graphql/controller/ProductController.java
+++ graphQLTech/src/main/java/com/tech/graphql/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.tech.graphql.controller;
 
 import com.tech.graphql.model.Product;
 import com.tech.graphql.service.ProductService;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/products")
 public class ProductController {
 
 
@@ -19,7 +19,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @QueryMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
